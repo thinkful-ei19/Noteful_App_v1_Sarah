2b145e180d5105d3062a26ab89e8354897448523
ssheaks <sarah.sheaks@gmail.com>
2018-03-05 13:55:21 -0800

    add app.get for notes and by id

--- server.js
+++ server.js
@@ -10,7 +10,17 @@ console.log('hello world!');
 const express = require('express');
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const note = data.find(note => note.id === id);
+  res.json(note);
+});
 
 // Listen for incoming connections
 app.listen(8080, function () {
